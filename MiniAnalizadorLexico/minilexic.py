9acdb746c5adf5847039a6d7875f2cb70c04b54d
JpMayagoitia <103715117+jpmfranco@users.noreply.github.com>
2024-01-21 12:53:17 -0600

    Add files via upload

--- MiniAnalizadorLexico/minilexic.py
+++ MiniAnalizadorLexico/minilexic.py
@@ -1,32 +1,38 @@
 def lexer(input_string):
     tokenactual = ""
 
-    
     for char in input_string:
         if char.isalpha() or char.isdigit() or char == ".":
             if char == '.':
-                tokenactual += int(char)
+                if tokenactual.count('.') == 0 and tokenactual.count('.') <=1:
+                    tokenactual += char
             else:
                 tokenactual += char
         else:
             if tokenactual:
                 if tokenactual.isdigit():
                     tokenactual = int(tokenactual)
-                    print(tokenactual,"\t\tReal")
-                elif tokenactual.isdigit() and tokenactual :
-                    print(tokenactual,"\t\tReal")
+                    print(tokenactual, "\t\tReal")
                 else:
-                    print(tokenactual,"    \tID")
+                    tokenactual = float(tokenactual) if '.' in tokenactual else print(tokenactual,"\t\tID")
+                    if tokenactual == None:
+                        tokenactual = ''
+                    else:
+                        print(tokenactual, "\t\tReal")
                 tokenactual = ""
+
 
     if tokenactual:
         if tokenactual.isdigit():
             tokenactual = int(tokenactual)
-            print(tokenactual,"\tEntero")
+            print(tokenactual, "\t\tReal")
         else:
-            print(tokenactual," \t\tID")
+            tokenactual = float(tokenactual) if '.' in tokenactual else print(tokenactual,"\t\tID")
+            if tokenactual == None:
+                print("")
+            else:
+                print(tokenactual, "\t\tReal")
 
 
-# Ejemplo de uso
-input_text = "entero 98.90 hola123  1982 hola como estas muy bi3n espero+87798+que_987673+muy()bien"
+input_text = "entero 98.94 hol123 1982 hola como estas muy bi3n espero+87798+que_987673+muy()bien_987.56"
 lexer(input_text)
